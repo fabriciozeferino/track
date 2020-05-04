c490607e996c2ffe3701bba106eef90b72c5b67f
Fabricio Zeferino <fabriciozeferino84@gmail.com>
2020-05-04 18:23:12 +0100

    Removes sign up

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,22 +1,68 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 import { NavigationEvents } from 'react-navigation';
 import { Context as AuthContext } from '../context/AuthContext';
-import AuthForm from '../components/AuthForm';
+import { FontAwesome } from '@expo/vector-icons';
 
-const SigninScreen = ({ navigation }) => {
-  const { state, signin, clearErrorMessage } = useContext(AuthContext);
+const SigninScreen = ({ errorMessage }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { signin, clearErrorMessage } = useContext(AuthContext);
 
   return (
     <View style={styles.container}>
       <NavigationEvents onWillFocus={clearErrorMessage} />
 
-      <AuthForm
-        headerText="Sign in to your account"
-        errorMessage={state.errorMessage}
-        submitButtonText="Sign In"
-        onSubmit={signin}
+      <Spacer>
+        <Text h3>Sign in</Text>
+      </Spacer>
+
+      <Spacer />
+
+      <Input
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoCorrect={false}
+        leftIcon={
+          <FontAwesome
+            name="user"
+            size={24}
+            color="grey"
+            style={{ marginEnd: 5 }}
+          />
+        }
       />
+
+      <Spacer />
+      <Input
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+        textContentType="none"
+        leftIcon={
+          <FontAwesome
+            name="lock"
+            size={24}
+            color="grey"
+            style={{ marginEnd: 5 }}
+          />
+        }
+      />
+
+      {errorMessage ? (
+        <Text style={styles.errorMessage}>{errorMessage}</Text>
+      ) : null}
+
+      <Spacer>
+        <Button title="Sign in" onPress={() => signin({ email, password })} />
+      </Spacer>
     </View>
   );
 };
@@ -34,5 +80,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginBottom: 250,
+  },
+  errorMessage: {
+    color: 'red',
+    marginLeft: 15,
+    marginTop: 15,
   },
 });
