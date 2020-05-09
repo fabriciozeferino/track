79dd122059e6e42522237a6c55fbe56d5bdc31c7
Fabricio Zeferino <fabriciozeferino84@gmail.com>
2020-05-09 08:05:49 +0100

    Refactoring signin done

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,11 +1,12 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { StyleSheet, View, Image, KeyboardAvoidingView } from 'react-native';
+import React, { useContext, useState, useEffect, useRef, useCallback } from 'react';
+import { StyleSheet, View, Image, KeyboardAvoidingView, ScrollView } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 import { NavigationEvents, SafeAreaView } from 'react-navigation';
 import { Context as AuthContext } from '../context/AuthContext';
 import { useForm } from 'react-hook-form';
 import { MaterialIcons } from '@expo/vector-icons';
+import { KeyboardAccessoryNavigation } from 'react-native-keyboard-accessory';
 
 const SigninScreen = ({ navigation }) => {
   const { register, setValue, handleSubmit, errors } = useForm();
@@ -17,19 +18,21 @@ const SigninScreen = ({ navigation }) => {
   } = useContext(AuthContext);
 
   const onSubmit = ({ email, password }) => {
-    console.log({ email, password });
     clearErrorMessage();
     signin({ email, password });
   };
 
-  const initialState = () => {
-    return {
-      focusDescriptionInput: false,
-    };
+  // Handle focus
+  const [activeInputFocus, setActiveInputFocus] = useState('emailRef');
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const handleFocusNext = () => {
+    activeInputFocus === 'emailRef' ? passwordRef.current.focus() : emailRef.current.focus();
   };
 
-  const handleTitleInputSubmit = () => {
-    this.setState({ focusDescriptionInput: true });
+  const handleFocusPrevious = () => {
+    activeInputFocus === 'emailRef' ? passwordRef.current.focus() : emailRef.current.focus();
   };
 
   // Register validation
@@ -39,72 +42,87 @@ const SigninScreen = ({ navigation }) => {
   }, [register]);
 
   return (
-    <SafeAreaView style={styles.container}>
-      <NavigationEvents onWillFocus={clearErrorMessage} />
+    <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
+        <NavigationEvents onWillFocus={clearErrorMessage} />
 
-      <KeyboardAvoidingView behavior="padding" style={styles.form}>
-        <Spacer>
-          <Text
-            h3
-            style={{
-              marginVertical: 25,
-              alignSelf: 'center',
-              fontWeight: '700',
+        <KeyboardAvoidingView behavior="padding" style={styles.form}>
+          <Image source={require('../assets/fw-logo.png')} />
+
+          <Spacer>
+            <Text
+              h3
+              style={{
+                marginVertical: 25,
+                alignSelf: 'center',
+                fontWeight: '700',
+              }}
+            >
+              Sign in
+            </Text>
+          </Spacer>
+
+          <Input
+            name="email"
+            onChangeText={(text) => {
+              clearErrorMessage();
+              setValue('email', text, true);
             }}
-          >
-            Sign in
-          </Text>
-        </Spacer>
+            autoCapitalize="none"
+            textContentType="emailAddress"
+            keyboardType="email-address"
+            autoFocus
+            autoCorrect={false}
+            errorMessage={errors.email && 'Email is required'}
+            returnKeyType="next"
+            blurOnSubmit={false}
+            onSubmitEditing={() => password.focus()}
+            placeholder="Email"
+            ref={emailRef}
+            onFocus={() => setActiveInputFocus('emailRef')}
+          />
 
-        <Input
-          name="email"
-          onChangeText={(text) => {
-            clearErrorMessage();
-            setValue('email', text, true);
-          }}
-          autoCapitalize="none"
-          textContentType="emailAddress"
-          keyboardType="email-address"
-          autoFocus={true}
-          autoCorrect={false}
-          errorMessage={errors.email && 'Email is required'}
-          returnKeyType="next"
-          blurOnSubmit={false}
-          onSubmitEditing={() => password.focus()}
-          placeholder="Email"
-        />
+          <Spacer />
+          <Input
+            name="password"
+            onChangeText={(text) => {
+              clearErrorMessage();
+              setValue('password', text, true);
+            }}
+            autoCapitalize="none"
+            autoCorrect={false}
+            secureTextEntry
+            textContentType="none"
+            errorMessage={errors.password && 'Password is required'}
+            blurOnSubmit={false}
+            ref={passwordRef}
+            onFocus={() => setActiveInputFocus('passwordRef')}
+            onSubmitEditing={handleSubmit(onSubmit)}
+            returnKeyType="done"
+            placeholder="Password"
+          />
 
-        <Spacer />
-        <Input
-          name="password"
-          onChangeText={(text) => {
-            clearErrorMessage();
-            setValue('password', text, true);
-          }}
-          autoCapitalize="none"
-          autoCorrect={false}
-          secureTextEntry
-          textContentType="none"
-          errorMessage={errors.password && 'Password is required'}
-          blurOnSubmit={false}
-          ref={(input) => (password = input)}
-          onSubmitEditing={handleSubmit(onSubmit)}
-          returnKeyType="done"
-          placeholder="Password"
-        />
+          {errorMessage ? (
+            <>
+              <View style={styles.alert}>
+                <MaterialIcons name="error-outline" size={24} color="#FF453A" />
+                <Text style={{ color: '#FF453A' }}>{errorMessage}</Text>
+              </View>
+            </>
+          ) : null}
 
-        {errorMessage ? (
-          <>
-            <View style={styles.alert}>
-              <MaterialIcons name="error-outline" size={24} color="#FF453A" />
-              <Text style={{ color: '#FF453A' }}>{errorMessage}</Text>
-            </View>
-          </>
-        ) : null}
-
-        <Button title="Sign in" style={{ marginHorizontal: 10, marginTop: 40 }} onPress={handleSubmit(onSubmit)} />
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+          <Button title="Sign in" style={{ marginHorizontal: 10, marginTop: 40 }} onPress={handleSubmit(onSubmit)} />
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+      <KeyboardAccessoryNavigation
+        nextDisabled={activeInputFocus === 'passwordRef' ? true : false}
+        previousDisabled={activeInputFocus === 'emailRef' ? true : false}
+        nextHidden={false}
+        previousHidden={false}
+        onNext={handleFocusNext}
+        onPrevious={handleFocusPrevious}
+      />
+    </View>
   );
 };
 
